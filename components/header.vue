00171b8cd37a83a8c9fd15491e52e86f4ece61e4
Longraf <32570545+Longraf@users.noreply.github.com>
2021-02-18 09:09:08 +0300

    Nuxt project build loaded

---
+++ components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    1111
+  </header>
+</template>
+
+<script>
+
+  export default {
+    name: 'header',
+    data() {
+      return {
+
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  .header-info-club{
+  }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'CarSelection',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'normalize.css/normalize.css',
+    '~assets/styles/main.scss',
+  ],
+
+  styleResources: {
+    scss: [
+      '~assets/styles/vars/*.scss',
+      '~assets/styles/vars/media.scss',
+      '~assets/styles/main.scss'
+    ]
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  /*
+  ** Svg sprite plugin options
+  */
+  svgSprite: {
+    input: '~/assets/images/icons',
+    output: '~/assets/images/icons-gen',
+  },
+  /*
+  ** Layout transitions options
+  */
+  layoutTransition: {
+    name: 'ease-opacity',
+    mode: 'out-in'
+  }
+}
